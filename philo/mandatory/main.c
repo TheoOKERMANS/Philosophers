68d1ac52909e3de4b832405565005576714fed4c
Theo OKERMANS <tokerman@e3r5p22.clusters.42paris.fr>
2022-11-21 00:37:02 +0100

    Fixe parsing

--- philo/mandatory/main.c
+++ philo/mandatory/main.c
@@ -6,35 +6,56 @@
 /*   By: tokerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 15:46:11 by tokerman          #+#    #+#             */
-/*   Updated: 2022/09/20 18:50:35 by tokerman         ###   ########.fr       */
+/*   Updated: 2022/11/21 00:17:23 by tokerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+int	test_args_value(char **argv)
+{
+	if (ft_atoi(argv[1]) < 2)
+	{
+		write(2, "Not enought philosophers\n", 25);
+		return (0);
+	}
+	return (1);
+}
 
 int	test_args(int argc, char **argv)
 {
 	int	i;
 
 	i = 1;
-	if (argc < 5 || argc > 6)
-		return (0);
 	while (i < argc)
 	{
 		if (all_num(argv[i]) == 0)
+		{
+			write(2, "Not a unsigned int\n", 13);
 			return (0);
+		}
 		if (ft_strlen(argv[i]) > 10)
+		{
+			write(2, "Number too long\n", 16);
 			return (0);
+		}
 		if (ft_atoi_long(argv[i]) > 2147483647)
+		{
+			write(2, "Number is highter than INT MAX\n", 31);
 			return (0);
+		}
 		i++;
 	}
-	return (1);
-	(void)argv;
+	return (test_args_value(argv));
 }
 
 int	main(int argc, char **argv)
 {
+	if (argc < 5 || argc > 6)
+	{
+		write(2, "Not enought arguments\n", 22);
+		return (0);
+	}
 	if (test_args(argc, argv) == 1)
 	{
 		if (argc == 6)
@@ -42,11 +63,5 @@ int	main(int argc, char **argv)
 		else if (argc == 5)
 			philo(argv, 2147483647);
 	}
-	else
-	{
-		write(2, "Error\n", 6);
-	}
 	return (1);
-	(void)argc;
-	(void)argv;
 }
